2fad6e56a4b308daee2af1f1bbd1d7605093e03d
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-05-24 20:42:48 +0200

    Adding Grpc DiscountUrl Configuration in Basket.API image configuration on DC

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
+using System;
+using Basket.API.GrpcServices;
 
 namespace Basket.API
 {
@@ -25,6 +28,11 @@ namespace Basket.API
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" }));
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+            
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+                options.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+                
+            services.AddScoped<DiscountGrpcService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
